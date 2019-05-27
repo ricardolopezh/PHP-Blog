c174aa7f66e4fa13c48961847725909aa66f91c6
Ricardo López <ricardo.lopezh97@gmail.com>
2019-05-27 01:00:52 -0500

    Remove unused CSS and JavaScipt files

--- footer.php
+++ footer.php
@@ -2,11 +2,10 @@
 	<p>Creado por Ricardo López &copy; <?php echo date('Y') ?> 
 		<a href="admin/nuevo.php" class="text-white mr-3">Crear nueva entrada</a>
 	</p>
-
 </footer>
 
 <script src="js/jquery.min.js"></script>
-<script src="js/bootstrap.js"></script>
+<script src="js/bootstrap.min.js"></script>
 <script src="js/app.js"></script>
 
 </body>
